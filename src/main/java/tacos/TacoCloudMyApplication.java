64791b9314ac69e45fb92b52e14b42bcddf8d6fe
Oleg Filatov <fillswim@gmail.com>
2022-07-02 13:15:00 +0300

    Initial commit

---
+++ src/main/java/tacos/TacoCloudMyApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudMyApplication.class, args);
+    }
+
+}
