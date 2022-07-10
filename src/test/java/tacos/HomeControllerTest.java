c10048c693680e6c69e8b5f17cfbc31fa782f650
Oleg Filatov <fillswim@gmail.com>
2022-07-10 10:40:47 +0300

    Update HomeControllerTest

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 class HomeControllerTest {
 
     @Autowired
